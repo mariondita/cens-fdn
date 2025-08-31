676e0d31bfaf4b54149b072961b38558659bec9b
mariondita <mariondita@gmail.com>
2025-08-31 20:30:20 +0200

    Remove all posts from homepage for clean layout

--- .eleventy.js
+++ .eleventy.js
@@ -49,7 +49,7 @@ module.exports = function(config) {
   config.addCollection('postFeed', collection => {
     return [...collection.getFilteredByGlob('./src/posts/*.md').filter(livePosts)]
       .reverse()
-      .filter(post => !post.inputPath.includes('/archive/'))
+      .filter(post => post.fileSlug === 'a-simple-post')
       .slice(0, site.maxPostsPerPage);
   });
 
